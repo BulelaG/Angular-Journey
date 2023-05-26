6b82e4ecdede829766525130171b69dd03f34785
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-05-26 16:55:33 +0200

    adding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,9 +8,10 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-
+  
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
@@ -21,4 +22,35 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
-}
+
+  add(hero_name: string, real_name: string, age: string, super_powers: string, power_origins:string, weakness:string, nationality:string ): void {
+    const HeroData = {
+      real_name,
+      hero_name,
+      age,
+      super_powers,
+      power_origins,
+      weakness,
+      nationality
+
+    }   
+    
+    hero_name = hero_name.trim();
+    if (!hero_name) { return; }
+    this.heroService.addHero(HeroData)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+        console.log(hero)
+      });
+
+     
+  }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
+
+
+ }
+  
